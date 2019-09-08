8e8ef6f8126b63454f6298357812bd89a7e60ff8
Jinjiang <zhaojinjiang@me.com>
2019-09-09 01:29:31 +0800

    Supported both cjs and esm bundles.

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,14 @@ import commonjs from "rollup-plugin-commonjs";
 import nodeResolve from "rollup-plugin-node-resolve";
 import vue from "rollup-plugin-vue";
 
-export default {
+const genConfig = (format = "cjs") => ({
   input: "index.js",
   output: {
-    file: "dist/vue-mark-display.js",
-    format: "esm"
+    file: `dist/vue-mark-display.${format}.js`,
+    format
   },
   external: ["vue"],
   plugins: [nodeResolve({ jsnext: true }), commonjs(), vue()]
-};
+});
+
+export default [genConfig("cjs"), genConfig("esm")];
